5c18e7e771b21651f0f2acde090feebf13d6db57
osamagucCitySoc <osamarabie@osamas-macbook-pro.local>
2015-12-30 20:56:17 +0300

    Initial Commit

---
+++ GroupFollowBack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GroupFollowBack
+//
+//  Created by Osama Rabie on 12/30/15.
+//  Copyright © 2015 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
